1230900f65f7214c8d8dba0e53aa73737c7dd2f3
allseenn <allseen@live.ru>
2024-10-03 00:34:42 +0300

    init

---
+++ 01.Lecture/dom.js
@@ -0,0 +1,18 @@
+// console.log(document.documentElement);
+// console.log(document.body);
+// console.log(document.head);
+// console.log(document.body.firstChild);
+// console.log(document.body.lastChild);
+// console.log(document.body.childNodes);
+// console.log(document.body.children);
+// for (let val of document.body.children) {
+//     console.log(val.localName === 'div' ? 'div' : 'not div');
+// }
+
+// for(let val of document.body.children) {
+//     console.log(val.nodeType);
+// }
+
+// for(let val of document.body.children) {
+//     console.log(val.nodeValue);
+// }
